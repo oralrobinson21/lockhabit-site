4ef6e448d94d35af9c6ed74572ebea0070e73e4d
oralrobinson21 <oralrobinson21@outlook.com>
2026-09-26 07:49:41 -0400

    Allow temporary moderation typing before Supabase type regeneration

--- src/routes/owner.journal-community.tsx
+++ src/routes/owner.journal-community.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { createFileRoute, Link } from "@tanstack/react-router";
 import { useEffect, useState, type FormEvent } from "react";
 
